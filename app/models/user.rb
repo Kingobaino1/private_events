e83a22dc52ae7f3c930d964e56384c28ea7b0dd3
martinnajjar12 <martin_najjar@yahoo.com>
2020-11-20 01:08:03 +0300

    Add name column to the User table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
+  has_many :attended_events, foreign_key: :attender_id, class_name: 'Event'
 end
