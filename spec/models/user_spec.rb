9b97d61475fe5538534a47710af22dc8a41c5dc3
Kingsley <kingobaino@gmail.com>
2020-11-24 23:46:34 +0100

    Write test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Should create a user' do
+    User.create(name: 'john', email: 'john@example.com', password: 'password')
+    expect(User.all.count).not_to eq(0)
+  end
+
+  it 'Should not create a user without name' do
+    user = User.create(email: 'john@example.com', password: 'password')
+    expect(user.id).to be nil
+  end
+
+  it 'Should not create a user without email' do
+    user = User.create(name: 'John', password: 'password')
+    expect(user.id).to be nil
+  end
+
+  it 'Should not create a user without password' do
+    user = User.create(name: 'Jon', email: 'john@example.com')
+    expect(user.id).to be nil
+  end
+
 end
