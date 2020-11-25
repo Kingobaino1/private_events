d95171cbec79750aba56c799a6629c14aa502dd2
martinnajjar12 <martin_najjar@yahoo.com>
2020-11-25 22:58:13 +0300

    Solve N + 1 Queries problem with current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # def current_user
+  #   @current_user ||= super && User.includes(:created_events).find(@current_user.id)
+  # end
+
   protected
   def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
